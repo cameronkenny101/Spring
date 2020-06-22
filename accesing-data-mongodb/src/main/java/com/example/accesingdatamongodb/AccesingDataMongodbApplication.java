215664f97cf2b275e010ff2c39575a25a6cd5e6b
cameronkenny101 <55995394+cameronkenny101@users.noreply.github.com>
2020-06-22 19:38:41 +0100

    MongoDB Queries

---
+++ accesing-data-mongodb/src/main/java/com/example/accesingdatamongodb/AccesingDataMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.accesingdatamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesingDataMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesingDataMongodbApplication.class, args);
+	}
+
+}
---
+++ accesing-data-mongodb/src/main/java/com/example/accesingdatamongodb/Customer.java
@@ -0,0 +1,4 @@
+package com.example.accesingdatamongodb;
+
+public class Customer {
+}
