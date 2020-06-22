b348be818a12d94a5667abb1374a374a736ec64d
cameronkenny101 <55995394+cameronkenny101@users.noreply.github.com>
2020-06-22 21:56:01 +0100

    Running Springboot from cmd line with maven

---
+++ maven_practice/src/test/java/com/example/maven_practice/MavenPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
