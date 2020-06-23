36695c371531682965c96cc1c5e32cab32375eea
cameronkenny101 <55995394+cameronkenny101@users.noreply.github.com>
2020-06-23 14:40:43 +0100

    Added Integration testing

---
+++ MongoDB-practice/src/main/java/com/example/MongoDBpractice/MongoDbPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.MongoDBpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbPracticeApplication.class, args);
+	}
+
+}
